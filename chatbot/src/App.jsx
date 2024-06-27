f3435f37cc55bc7a60c99a972bba77701d5c5af3
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2024-06-26 21:07:05 -0400

    setting up a Chat page before I actually get started so i dont run into issues

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import Chat from "./views/Chat";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Welcome to your AI Chatbot</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
